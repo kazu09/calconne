90dc884487c20661fe0d4eafbceb92299e68841f
K.Kazu <64839248+kazu09@users.noreply.github.com>
2023-11-10 00:19:35 +0900

    Implementing a chat feature.

--- calconne-client/src/main.js
+++ calconne-client/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import BootstrapVueNext from 'bootstrap-vue-next'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+const app = createApp(App)
+app.use(BootstrapVueNext)
 createApp(App).mount('#app')
